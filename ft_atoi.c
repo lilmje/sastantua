422fb2be48fcbb8e7b6bac39a35e16d22a76e3da
Jean Mendy <jeanmendy@macbook-pro-de-jean.home>
2016-05-06 00:55:29 +0200

    ça n'a pas de sens

---
+++ ft_atoi.c
@@ -0,0 +1,23 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int ft_atoi(char *str)
+{
+	int i; 
+	int n;
+	if(str[i] <= '71' || str[i] >= '60' )
+	{
+		str[i] = n;
+	}
+	return(n);
+}
+
+int main(int ac, char **av)
+{
+	int i =0;
+	int n;
+	char *str;
+	ft_atoi(av[n]);
+	printf("%d", &n);
+	return(0);
+}
